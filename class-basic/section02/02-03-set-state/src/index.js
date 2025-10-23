c6a155757a1bec25f6372769bd60deacabdfc3d8
pepper-sister <yuyu7123@naver.com>
2025-10-24 01:22:32 +0900

    section02

--- class-basic/section02/02-03-set-state/src/index.js
+++ class-basic/section02/02-03-set-state/src/index.js
@@ -8,12 +8,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Aaa from './routes/aaa';
 import Bbb from './routes/bbb';
 import Board from './routes/board1';
+import BoardRefactoring from './routes/board2-refactoring';
 
 const 철수의페이지목록 = createBrowserRouter([
     { path: '/', element: <App /> },
     { path: '/aaa', element: <Aaa /> },
     { path: '/bbb', element: <Bbb /> },
     { path: '/board1', element: <Board /> },
+    { path: '/board2-refactoring', element: <BoardRefactoring /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
