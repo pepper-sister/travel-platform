5dc2f6e1f8ed47bd679b5ad551db316becdf2342
pepper-sister <yuyu7123@naver.com>
2025-10-24 02:01:32 +0900

    feat: 컴포넌트 라우팅 연결

--- homework/homework03/src/index.js
+++ homework/homework03/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import BoardsNew from './routes/boards/new/BoardsNew';
+import BoardsDetail from './routes/boards/new/BoardsDetail';
 
-const pages = createBrowserRouter([{ path: '/', element: <App /> }]);
+const pages = createBrowserRouter([
+    { path: '/', element: <App /> },
+    { path: '/boards/new', element: <BoardsNew /> },
+    { path: '/boards/detail', element: <BoardsDetail /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={pages} />);
