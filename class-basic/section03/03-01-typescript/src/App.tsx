883031094637453f91c0b9954a4a13a97c61eca1
pepper-sister <yuyu7123@naver.com>
2025-10-26 22:06:24 +0900

    section03

---
+++ class-basic/section03/03-01-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <button>버튼</button>
+        </div>
+    );
+}
+
+export default App;
