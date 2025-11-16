8f929e0fd1afbb8f533f2bc1a38b21571fa1d8ab
pepper-sister <yuyu7123@naver.com>
2025-11-16 23:33:02 +0900

    section07

---
+++ class-example/my-app/src/app/section07/07-02-static-routing-board-moved/2/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const FETCH_BOARD = gql`
+  query {
+    fetchBoard(number: 81) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARD);
+
+  console.log(data);
+
+  return (
+    <>
+      <div>2번 게시글 상세페이지 이동이 완료되었습니다.</div>
+      <div>작성자: {data && data.fetchBoard.writer}</div>
+      <div>제목: {data?.fetchBoard.title}</div>
+      <div>내용: {data ? data.fetchBoard.contents : ""}</div>
+    </>
+  );
+}
---
+++ class-example/my-app/src/app/section07/07-02-static-routing-board-moved/3/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const FETCH_BOARD = gql`
+  query {
+    fetchBoard(number: 82) {
+      number
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const { data } = useQuery(FETCH_BOARD);
+
+  console.log(data);
+
+  return (
+    <>
+      <div>3번 게시글 상세페이지 이동이 완료되었습니다.</div>
+      <div>작성자: {data && data.fetchBoard.writer}</div>
+      <div>제목: {data?.fetchBoard.title}</div>
+      <div>내용: {data ? data.fetchBoard.contents : ""}</div>
+    </>
+  );
+}
