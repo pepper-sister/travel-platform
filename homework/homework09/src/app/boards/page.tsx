66626d6c10d30edc1479027151185e74117d5fb4
pepper-sister <yuyu7123@naver.com>
2025-11-23 23:22:45 +0900

    feat: 목록 데이터 조회

--- homework/homework09/src/app/boards/page.tsx
+++ homework/homework09/src/app/boards/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+
+import { useQuery } from "@apollo/client/react";
 import styles from "./styles.module.css";
+import { gql } from "@apollo/client";
+import { useRouter } from "next/navigation";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards(page: 1) {
+      _id
+      title
+      writer
+      createdAt
+    }
+  }
+`;
 
 export default function BoardsList() {
+  const { data } = useQuery(FETCH_BOARDS);
+  const router = useRouter();
+
+  const onClickDetail = (boradId: string) => {
+    router.push(`/boards/${boradId}`);
+  };
+
   return (
     <div className={styles.list__section}>
       <div className="column__sort gap__8">
@@ -14,6 +37,28 @@ export default function BoardsList() {
             <p className={`${styles.list__subtitle__100} l__20 c__1C1C1C`}>작성자</p>
             <p className={`${styles.list__subtitle__100} l__20 c__1C1C1C`}>날짜</p>
           </div>
+        </div>
+
+        <div className="column__sort gap__12">
+          {data?.fetchBoards.map((el, index) => (
+            <div
+              key={el._id}
+              onClick={() => onClickDetail(el._id)}
+              className={`${styles.list__list} row__sort row__between`}
+            >
+              <div className="row__sort gap__8">
+                <p className={`${styles.list__subtitle__64} f__14 w__300 l__20 c__919191`}>{index}</p>
+                <p className="f__14 l__20 c__1C1C1C">{el.title}</p>
+              </div>
+
+              <div className="row__sort gap__8">
+                <p className={`${styles.list__subtitle__100} f__14 w__300 l__20 c__333333`}>{el.writer}</p>
+                <p className={`${styles.list__subtitle__100} f__14 w__300 l__20 c__919191`}>
+                  {el.createdAt.slice(0, 10)}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
