7a56b1e946bc0cec552a497242d9bfe836ffbb9d
pepper-sister <yuyu7123@naver.com>
2025-11-25 21:51:19 +0900

    chore: section09 과제 생성

---
+++ homework/homework10/src/app/boards/[boardId]/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./styles.module.css";
+import { useParams } from "next/navigation";
+import { useQuery } from "@apollo/client/react";
+import { gql } from "@apollo/client";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($userboardId: ID!) {
+    fetchBoard(boardId: $userboardId) {
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const BoardsDetail = () => {
+  const params = useParams();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { userboardId: params.boardId },
+  });
+
+  return (
+    <div className="column__sort gap__24">
+      <header className="f__28 w__700 l__36">{data?.fetchBoard.title}</header>
+
+      <main className="column__sort gap__24">
+        <div className="column__sort gap__16">
+          <div className="row__sort row__between column__center">
+            <div className="row__sort gap__4 column__center">
+              <Image
+                className={styles.person__img}
+                src="/images/person.png"
+                alt="person"
+                width={24}
+                height={24}
+                sizes="100vw"
+              />
+              <p className="f__14 w__300 l__20 c_5F5F5F">{data?.fetchBoard.writer}</p>
+            </div>
+            <p className="f__14 w__300 l__20 c__818181">2024.11.11</p>
+          </div>
+          <div className="div"></div>
+          <div className="row__sort row__end gap__8">
+            <Image src="/images/link.png" alt="link" width={24} height={24} sizes="100vw" />
+            <Image src="/images/location.png" alt="location" width={24} height={24} sizes="100vw" />
+          </div>
+        </div>
+
+        <Image src="/images/beach.jpg" alt="beach" width={400} height={531} sizes="100vw" />
+
+        <p className="w__400">{data?.fetchBoard.contents}</p>
+
+        <div className={`${styles.houseplants} row__sort row__center`}>
+          <Image
+            className={styles.houseplants__img}
+            src="/images/houseplants.jpg"
+            alt="houseplants"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+          <Image
+            className={`${styles.play} click`}
+            src="/images/play.png"
+            alt="play"
+            width={24}
+            height={24}
+            sizes="100vw"
+          />
+        </div>
+
+        <div className="row__sort row__center gap__24">
+          <div className="column__sort column__center gap__4">
+            <Image src="/images/bad.png" alt="bad" width={24} height={24} sizes="100vw" />
+            <p className="f__14 w__400 l__20 c_5F5F5F">24</p>
+          </div>
+          <div className="column__sort column__center gap__4">
+            <Image src="/images/good.png" alt="god" width={24} height={24} sizes="100vw" />
+            <p className="f__14 w__400 l__20 c__f66a6a">12</p>
+          </div>
+        </div>
+      </main>
+
+      <footer className="row__sort row__center gap__24">
+        <button className={`${styles.white__btn} white__btn row__sort column__center gap__8`}>
+          <Image src="/images/menu.png" alt="menu" width={24} height={24} sizes="100vw" />
+          <p className="f__14 w__600 l__20">목록으로</p>
+        </button>
+        <button className={`${styles.white__btn} white__btn row__sort column__center gap__8`}>
+          <Image src="/images/edit.png" alt="edit" width={24} height={24} sizes="100vw" />
+          <p className="f__14 w__600 l__20">수정하기</p>
+        </button>
+      </footer>
+    </div>
+  );
+};
+
+export default BoardsDetail;
---
+++ homework/homework10/src/app/boards/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useMutation, useQuery } from "@apollo/client/react";
+import Image from "next/image";
+import styles from "./styles.module.css";
+import { gql } from "@apollo/client";
+import { useRouter } from "next/navigation";
+
+const FETCH_BOARDS = gql`
+  query {
+    fetchBoards(page: 1) {
+      _id
+      title
+      writer
+      createdAt
+    }
+  }
+`;
+
+const DELETE_BOARD = gql`
+  mutation deleteBoard($userBoardId: ID!) {
+    deleteBoard(boardId: $userBoardId)
+  }
+`;
+
+export default function BoardsList() {
+  const { data } = useQuery(FETCH_BOARDS);
+  const [deleteBoard] = useMutation(DELETE_BOARD);
+
+  const router = useRouter();
+
+  const onClickDetail = (boardId: string) => {
+    router.push(`/boards/${boardId}`);
+  };
+
+  const onClickDelete = (boardId: string) => {
+    deleteBoard({
+      variables: {
+        userBoardId: boardId,
+      },
+      refetchQueries: [{ query: FETCH_BOARDS }],
+    });
+  };
+
+  return (
+    <div className={styles.list__section}>
+      <div className="column__sort gap__8">
+        <div className={`${styles.list__title} row__sort row__between`}>
+          <div className="row__sort gap__8">
+            <p className={`${styles.list__subtitle__64} l__20 c__1C1C1C`}>번호</p>
+            <p className="l__20 c__1C1C1C">제목</p>
+          </div>
+
+          <div className="row__sort gap__8">
+            <p className={`${styles.list__subtitle__100} l__20 c__1C1C1C`}>작성자</p>
+            <p className={`${styles.list__subtitle__100} l__20 c__1C1C1C`}>날짜</p>
+          </div>
+        </div>
+
+        <div className="column__sort gap__12">
+          {data?.fetchBoards.map((el, index) => (
+            <div key={el._id} className={`${styles.list__list} row__sort row__between`}>
+              <div onClick={() => onClickDetail(el._id)} className="click row__sort gap__8">
+                <p className={`${styles.list__subtitle__64} f__14 w__300 l__20 c__919191`}>{index}</p>
+                <p className="f__14 l__20 c__1C1C1C">{el.title}</p>
+              </div>
+
+              <div className="row__sort gap__8">
+                <p className={`${styles.list__subtitle__100} f__14 w__300 l__20 c__333333`}>{el.writer}</p>
+                <p className={`${styles.list__subtitle__100} f__14 w__300 l__20 c__919191`}>
+                  {el.createdAt.slice(0, 10)}
+                </p>
+                <Image
+                  onClick={() => onClickDelete(el._id)}
+                  className={`${styles.img__delete} click`}
+                  src="/images/delete.png"
+                  alt="삭제"
+                  width={24}
+                  height={0}
+                  sizes="100vw"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
