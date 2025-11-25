56a10e33a70d8b93546281eca201aa777d4dc4f3
pepper-sister <yuyu7123@naver.com>
2025-11-25 23:36:18 +0900

    refactor: 쿼리 분리

--- homework/homework11/src/app/boards/[boardId]/edit/page.tsx
+++ homework/homework11/src/app/boards/[boardId]/edit/page.tsx
@@ -1,20 +1,9 @@
 "use client";
 
 import BoardsWrite from "@/components/boards-write";
-import { gql } from "@apollo/client";
+import { FETCH_BOARD } from "@/components/boards-write/queries";
 import { useQuery } from "@apollo/client/react";
 import { useParams } from "next/navigation";
-
-const FETCH_BOARD = gql`
-  query fetchBoard($userboardId: ID!) {
-    fetchBoard(boardId: $userboardId) {
-      _id
-      writer
-      title
-      contents
-    }
-  }
-`;
 
 export default function BoardsEdit() {
   const params = useParams();
--- homework/homework11/src/components/boards-write/queries.ts
+++ homework/homework11/src/components/boards-write/queries.ts
@@ -1,5 +1,16 @@
 import { gql } from "@apollo/client";
 
+export const FETCH_BOARD = gql`
+  query fetchBoard($userboardId: ID!) {
+    fetchBoard(boardId: $userboardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
 export const CREATE_BOARD = gql`
   mutation createBoard($createBoardInput: CreateBoardInput!) {
     createBoard(createBoardInput: $createBoardInput) {
