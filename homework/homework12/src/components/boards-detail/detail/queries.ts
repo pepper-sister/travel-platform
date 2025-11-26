9cad208340d74534230e43ef17c944bdbed94215
pepper-sister <yuyu7123@naver.com>
2025-11-27 05:57:43 +0900

    feat: 쿼리 작성

--- homework/homework12/src/components/boards-detail/detail/queries.ts
+++ homework/homework12/src/components/boards-detail/detail/queries.ts
@@ -1,12 +1,20 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_BOARD = gql`
-  query fetchBoard($userboardId: ID!) {
-    fetchBoard(boardId: $userboardId) {
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
       _id
       writer
       title
       contents
+    }
+  }
+`;
+
+export const FETCH_COMMENT = gql`
+  query fetchBoardComments($boardId: ID!) {
+    fetchBoardComments(boardId: $boardId) {
+      _id
     }
   }
 `;
