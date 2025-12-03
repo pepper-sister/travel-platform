0ecff9720801ba76ea512cf1a64502cb0bd5900d
pepper-sister <yuyu7123@naver.com>
2025-12-03 23:01:52 +0900

    section12

--- class-example/my-app/src/app/section12/12-02-library-icon-typescript/page.tsx
+++ class-example/my-app/src/app/section12/12-02-library-icon-typescript/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { CloseCircleOutlined } from "@ant-design/icons";
+import { MouseEvent } from "react";
 
 export default function LibraryIconPage() {
   // 1. 대부분의 아이콘 라이브러리들은 <span />를 부모로 하여 내부에 아이콘 이미지가 자식으로 들어감
   // 2. 이미지 클릭시, 부모로 onClick 이벤트가 전파됨
-  const onClickDelete = (event) => {
+  const onClickDelete = (event: MouseEvent<HTMLSpanElement>) => {
     // alert(`${event.target.id}를 정말로 삭제합니까?`); // 비어있음
     alert(`${event.currentTarget.id}를 정말로 삭제합니까?`); // 정상작동
   };
