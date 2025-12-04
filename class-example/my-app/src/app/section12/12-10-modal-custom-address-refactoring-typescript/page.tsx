3cb753bee709bcbf261a4bf7e0231bfc22908b48
pepper-sister <yuyu7123@naver.com>
2025-12-04 20:12:52 +0900

    section12

---
+++ class-example/my-app/src/app/section12/12-10-modal-custom-address-refactoring-typescript/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { Modal } from "antd";
+import DaumPostcodeEmbed, { Address } from "react-daum-postcode";
+
+export default function ModalCustomPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggleModal = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleComplete = (data: Address) => {
+    console.log(data);
+    onToggleModal();
+  };
+
+  return (
+    <>
+      <button onClick={onToggleModal}>모달창 열기!!</button>
+      {/* 모달 종료 방식 - 1. 모달 숨기는 방법(ex, 게시글, 이력서 등) */}
+      {/* <Modal open={isOpen} onOk={handleOk} onCancel={handleCancel}>
+        게시글 내용 입력: <input type="text" />
+      </Modal> */}
+
+      {/* 모달 종료 방식 - 2. 모달 삭제하는 방법(ex, 신용카드, 비밀번호 등) */}
+      {isOpen && (
+        <Modal open={true} onOk={onToggleModal} onCancel={onToggleModal}>
+          <DaumPostcodeEmbed onComplete={handleComplete} />
+        </Modal>
+      )}
+    </>
+  );
+}
