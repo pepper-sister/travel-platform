2bfe59c4a89ebc85e9504b56aac737f079555fb8
pepper-sister <yuyu7123@naver.com>
2025-12-05 00:40:23 +0900

    chore: 주소 타입 작성

--- homework/homework14/src/components/boards-write/types.ts
+++ homework/homework14/src/components/boards-write/types.ts
@@ -1,8 +1,15 @@
 import { FetchBoardQuery } from "@/commons/graphql/graphql";
 
+export interface BoardAddressInput {
+  zipcode?: string;
+  address?: string;
+  addressDetail?: string;
+}
+
 export interface IBoardWriteInputChange {
   title?: string;
   contents?: string;
+  boardAddress?: BoardAddressInput;
 }
 
 export interface IBoardWriteData {
