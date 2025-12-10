4386b0ba282f8b93080c44b09d770560e4fbdde1
pepper-sister <yuyu7123@naver.com>
2025-12-10 23:59:40 +0900

    section15

---
+++ class-practice/my-app/src/app/section15/15-02-comment-edit1/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($mypage: Int) {
+    fetchBoards(page: $mypage) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function PaginationPage() {
+  const [myIndex, setMyIndex] = useState(-1);
+  const { data } = useQuery(FETCH_BOARDS);
+
+  const onClickEdit = (event) => {
+    setMyIndex(Number(event.currentTarget.id));
+  };
+
+  return (
+    <>
+      {data?.fetchBoards.map((el, index) =>
+        myIndex !== index ? (
+          <div key={el._id}>
+            <span style={{ margin: "10px" }}>{el.title}</span>
+            <span style={{ margin: "10px" }}>{el.writer}</span>
+            <button id={index} onClick={onClickEdit}>
+              수정하기
+            </button>
+          </div>
+        ) : (
+          <input type="text" key={el._id} />
+        )
+      )}
+    </>
+  );
+}
