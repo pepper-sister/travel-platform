efee2fdbbd423bd16818f8c6994e0b11b4bfa8ad
pepper-sister <yuyu7123@naver.com>
2025-12-14 21:15:40 +0900

    refactor: 기능 hook파일로 이동

--- homework/homework17/src/components/boards-detail/comment-list/hook.ts
+++ homework/homework17/src/components/boards-detail/comment-list/hook.ts
@@ -3,11 +3,33 @@
 import { FetchBoardCommentsDocument } from "@/commons/graphql/graphql";
 import { useQuery } from "@apollo/client/react";
 import { IFetchCommentData } from "./types";
+import { useState } from "react";
 
 export const useCommentList = (props: IFetchCommentData) => {
+  const [hasMore, setHasMore] = useState(true);
   const { data, fetchMore } = useQuery(FetchBoardCommentsDocument, {
     variables: { boardId: String(props.params.boardId) },
   });
 
-  return { data, fetchMore };
+  const onNext = () => {
+    if (data === undefined) return;
+
+    fetchMore({
+      variables: {
+        page: Math.ceil(data?.fetchBoardComments.length / 10) + 1,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult.fetchBoardComments.length) {
+          setHasMore(false);
+          return prev;
+        }
+
+        return {
+          fetchBoardComments: [...prev.fetchBoardComments, ...fetchMoreResult.fetchBoardComments],
+        };
+      },
+    });
+  };
+
+  return { data, onNext, hasMore };
 };
