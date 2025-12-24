ccb1df80fb5bac530f51e48128dd696f5a02ef98
pepper-sister <yuyu7123@naver.com>
2025-12-24 23:48:12 +0900

    fix: 이미지 태그 변경

--- homework/homework18/src/app/openapis/page.tsx
+++ homework/homework18/src/app/openapis/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import { useOpenAPI } from "@/components/openapis-list/hook";
-import Image from "next/image";
 
 export default function CatPicture() {
   const { imageUrl } = useOpenAPI();
 
   return (
     <>
-      <Image src={imageUrl} alt="강아지" width={0} height={0} sizes="100vw" />
+      <img src={imageUrl} />
     </>
   );
 }
