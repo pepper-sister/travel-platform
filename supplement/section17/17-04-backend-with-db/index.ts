3bf9e85905f0a2498363f4f5d05fa667bf9286c9
pepper-sister <yuyu7123@naver.com>
2025-12-28 23:53:24 +0900

    section17

--- supplement/section17/17-04-backend-with-db/index.ts
+++ supplement/section17/17-04-backend-with-db/index.ts
@@ -4,8 +4,29 @@ import { Board } from "./Board.postgres";
 console.log("백엔드 프로그램을 실행합니다.");
 
 console.log("여기서 API를 만들거예요");
-// ..
-// ..
+// 1. API-DOCS 만들기
+const 나의API독스 = `#graphql
+  type MyBoard {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type Query {
+    fetchBoards: [MyBoard]
+  }
+
+  type Mutation {
+    createBoard: String
+  }
+`;
+
+// 2. API 만들기
+
+// 3. API-DOCS와 API를 그룹핑한 아폴로서버 만들기
+
+// 4. 최종 완성된 아폴로서버 실행시키기(리스닝하기 => 대기하기)
 
 console.log("여기서 DB에 접속하고, 테이블을 만들거예요");
 export const AppDataSource = new DataSource({
