19802f72bcdb06ce7e09bab4a0d684aca0ec35e5
pepper-sister <yuyu7123@naver.com>
2026-01-06 17:04:30 +0900

    feat: 이미지 등록 기능 추가

---
+++ homework/homework20/src/components/boards-write/queries.ts
@@ -0,0 +1,42 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+      youtubeUrl
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+    }
+  }
+`;
+
+export const CREATE_BOARD = gql`
+  mutation createBoard($createBoardInput: CreateBoardInput!) {
+    createBoard(createBoardInput: $createBoardInput) {
+      _id
+    }
+  }
+`;
+
+export const UPDATE_BOARD = gql`
+  mutation updateBoard($updateBoardInput: UpdateBoardInput!, $password: String, $boardId: ID!) {
+    updateBoard(updateBoardInput: $updateBoardInput, password: $password, boardId: $boardId) {
+      _id
+    }
+  }
+`;
+
+export const UPLOAD_FILE = gql`
+  mutation uplaodFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
