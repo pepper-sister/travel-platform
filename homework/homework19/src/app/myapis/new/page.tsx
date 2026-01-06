2a640ec5a644e23588f48794ae5adafbd9b82292
pepper-sister <yuyu7123@naver.com>
2026-01-06 13:19:43 +0900

    refactor: 훅, ui 분리

--- homework/homework19/src/app/myapis/new/page.tsx
+++ homework/homework19/src/app/myapis/new/page.tsx
@@ -1,45 +1,7 @@
 "use client";
 
-import { firebaseApp } from "@/commons/libraries/firebase";
-import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
-import { ChangeEvent, useState } from "react";
+import MyApisWriteUI from "@/components/myapis-write";
 
-export default function MyApisSubmitPage() {
-  const [inputs, setInputs] = useState({
-    writer: "",
-    title: "",
-    contents: "",
-  });
-
-  const onChangeInputs = async (event: ChangeEvent<HTMLInputElement>) => {
-    const boards = collection(getFirestore(firebaseApp), "boards");
-    const result = await getDocs(boards);
-    const data = result.docs.map((el) => el.data());
-
-    const userInputs = {
-      number: data.length + 1,
-      ...inputs,
-      [event.target.id]: event.target.value,
-    };
-
-    setInputs(userInputs);
-  };
-
-  const onClickSubmit = async () => {
-    const boards = collection(getFirestore(firebaseApp), "boards");
-    await addDoc(boards, inputs);
-  };
-
-  return (
-    <>
-      <div>등록페이지</div>
-      작성자: <input id="writer" type="text" onChange={onChangeInputs}></input>
-      <br />
-      제목: <input id="title" type="text" onChange={onChangeInputs}></input>
-      <br />
-      내용: <input id="contents" type="text" onChange={onChangeInputs}></input>
-      <br />
-      <button onClick={onClickSubmit}>등록하기</button>
-    </>
-  );
+export default function MyApisWritePage() {
+  return <MyApisWriteUI />;
 }
