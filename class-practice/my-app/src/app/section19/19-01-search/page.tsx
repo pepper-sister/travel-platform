6317f53df5e2599352475c9b2878fdfb88beb9a8
pepper-sister <yuyu7123@naver.com>
2026-01-07 17:34:05 +0900

    section19

---
+++ class-practice/my-app/src/app/section19/19-01-search/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($mypage: Int) {
+    fetchBoards(page: $mypage) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function PaginationPage() {
+  const { data, refetch } = useQuery(FETCH_BOARDS);
+
+  console.log(data);
+
+  const onClickPage = (event) => {
+    refetch({ mypage: Number(event.currentTarget.id) });
+  };
+
+  return (
+    <>
+      검색어입력: <input type="text" />
+      <button>검색하기</button>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill("철수").map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+          {index + 1}
+        </span>
+      ))}
+    </>
+  );
+}
