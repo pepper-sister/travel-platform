2387f87532ab394e2337ffd104e20ca48be2c545
pepper-sister <yuyu7123@naver.com>
2026-01-09 22:18:55 +0900

    section19

---
+++ class-practice/my-app/src/app/section19/19-04-search-with-debouncing-keyword/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
+import _ from "lodash";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($mypage: Int, $mysearch: String) {
+    fetchBoards(page: $mypage, search: $mysearch) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function PaginationPage() {
+  // const [search, setSearch] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  const { data, refetch } = useQuery(FETCH_BOARDS);
+
+  console.log(data);
+
+  const onClickPage = (event) => {
+    // 검색에서 refetch할 때, search 검색어가 refetch에 저장되어 있는 상태이므로, 여기서 굳이 추가 안해도 됨
+    refetch({ mypage: Number(event.currentTarget.id) });
+  };
+
+  const getDebounce = _.debounce((value) => {
+    refetch({ mysearch: value, mypage: 1 });
+    setKeyword(value);
+  }, 500);
+
+  const onChangeSearch = (event) => {
+    getDebounce(event.target.value);
+  };
+
+  // const onClickSearch = () => {
+  //   refetch({ mysearch: search, mypage: 1 });
+  // };
+
+  return (
+    <>
+      검색어입력: <input type="text" onChange={onChangeSearch} />
+      {/* <button onClick={onClickSearch}>검색하기</button> */}
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>
+            {el.title
+              .replaceAll(keyword, `@#$${keyword}@#$`)
+              .split("@#$")
+              .map((el, index) => (
+                <span key={`${el}_${index}`} style={{ color: el === keyword ? "red" : "black" }}>
+                  {el}
+                </span>
+              ))}
+          </span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+      {new Array(10).fill("철수").map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
+          {index + 1}
+        </span>
+      ))}
+    </>
+  );
+}
