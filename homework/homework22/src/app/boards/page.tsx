9d92fbb7666410454fae50297b0bb5df12adad0e
pepper-sister <yuyu7123@naver.com>
2026-01-15 01:34:46 +0900

    style: body, navi style 수정

---
+++ homework/homework22/src/app/boards/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { FetchBoardsDocument } from "@/commons/graphql/graphql";
+import BoardsListUI from "@/components/boards-list/list";
+import PaginationUI from "@/components/boards-list/pagination";
+import SearchUI from "@/components/boards-list/search";
+import { useQuery } from "@apollo/client/react";
+import { useState } from "react";
+
+export default function BoardsList() {
+  const [keyword, setKeyword] = useState("");
+  const { data, refetch } = useQuery(FetchBoardsDocument, {
+    variables: {
+      page: 1,
+    },
+  });
+
+  const onRefetch = (page: number) => {
+    refetch({ page });
+  };
+
+  return (
+    <div className="body__sort">
+      <div className="body column__sort gap__24">
+        <div className="f__28 w__700 l__36">트립토크 게시판</div>
+        <SearchUI refetch={refetch} setKeyword={setKeyword} />
+        <div className="column__sort gap__8 list__section">
+          <BoardsListUI data={data} keyword={keyword} />
+          <PaginationUI onRefetch={onRefetch} />
+        </div>
+      </div>
+    </div>
+  );
+}
