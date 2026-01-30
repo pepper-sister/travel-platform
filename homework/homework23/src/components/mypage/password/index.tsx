6d73baa168e2073f6d6cdbdd87fa9cf775874d03
pepper-sister <yuyu7123@naver.com>
2026-01-30 22:09:01 +0900

    refactor: 훅 분리

--- homework/homework23/src/components/mypage/password/index.tsx
+++ homework/homework23/src/components/mypage/password/index.tsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
+import { usePassword } from "./hook";
 import styles from "./styles.module.css";
 import { Modal } from "antd";
 
 export default function PasswordUI() {
-  const [passwordInput, setPasswordInput] = useState("");
-  const [passwordInputCheck, setPasswordInputCheck] = useState("");
-  const [password, setPassword] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const onChangePasswordInput = (event) => {
-    setPasswordInput(event.target.value);
-
-    if (event.target.value && passwordInputCheck) setPassword(false);
-    else setPassword(true);
-  };
-
-  const onChangePasswordInputCheck = (event) => {
-    setPasswordInputCheck(event.target.value);
-
-    if (passwordInput && event.target.value) setPassword(false);
-    else setPassword(true);
-  };
-
-  const showPasswordModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
+  const { password, isModalOpen, onChangePasswordInput, onChangePasswordInputCheck, showPasswordModal, handleOk } =
+    usePassword();
 
   return (
     <div className="column__sort column__right gap__24">
