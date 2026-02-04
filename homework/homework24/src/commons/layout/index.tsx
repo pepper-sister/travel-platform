cc3c9041a5eb100e2db6c8d03bf90f78e1c8a207
pepper-sister <yuyu7123@naver.com>
2026-02-04 23:54:01 +0900

    feat: 회원가입, 로그인 페이지에서 배너와 내비게이션 숨기기

--- homework/homework24/src/commons/layout/index.tsx
+++ homework/homework24/src/commons/layout/index.tsx
@@ -9,14 +9,16 @@ interface ILayout {
 }
 export default function Layout({ children }: ILayout) {
   const params = useParams();
-  const HIDDEN_BANNER = ["/boards/new", `/boards/${params.boardId}/edit`, `/mypage`];
+  const HIDDEN_NAVIGATION = ["/sign"];
+  const HIDDEN_BANNER = ["/boards/new", `/boards/${params.boardId}/edit`, "/mypage", "/sign"];
 
   const pathname = usePathname();
+  const isHiddenNavigation = HIDDEN_NAVIGATION.includes(pathname);
   const isHiddenBanner = HIDDEN_BANNER.includes(pathname);
 
   return (
     <>
-      <NavigationUI />
+      {!isHiddenNavigation && <NavigationUI />}
       {!isHiddenBanner && <Banner />}
       {children}
     </>
