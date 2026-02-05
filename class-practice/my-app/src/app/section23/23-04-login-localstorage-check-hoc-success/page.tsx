b004304303a7bc99b63a192651cfa80b01f1a262
pepper-sister <yuyu7123@naver.com>
2026-02-05 21:20:09 +0900

    section23

---
+++ class-practice/my-app/src/app/section23/23-04-login-localstorage-check-hoc-success/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { with로그인체크 } from "@/commons/hocs/23-04-with로그인체크";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const FETCH_USER_LOGGED_IN = gql`
+  query fetchUserLoggedIn {
+    fetchUserLoggedIn {
+      _id
+      email
+      name
+    }
+  }
+`;
+
+function LoginSuccessPage() {
+  const { data } = useQuery(FETCH_USER_LOGGED_IN);
+
+  return <>{data?.fetchUserLoggedIn.name}님 환영합니다!</>;
+}
+
+export default with로그인체크(LoginSuccessPage);
---
+++ class-practice/my-app/src/app/section23/23-04-login-localstorage-check-hoc/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useAccessTokenStore } from "@/commons/stores/22-01-access-token-store";
+import { gql } from "@apollo/client";
+import { useMutation } from "@apollo/client/react";
+import { useRouter } from "next/navigation";
+
+const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [loginUser] = useMutation(LOGIN_USER);
+  const { setAccessToken } = useAccessTokenStore();
+
+  const onClickLogin = async () => {
+    // 1. 로그인 뮤테이션 날려서 accessToken 받아오기
+    const result = await loginUser({
+      variables: {
+        email: "suzi@d.com",
+        password: "1234",
+      },
+    });
+    const accessToken = result.data.loginUser.accessToken;
+    console.log(accessToken);
+
+    // 2. 받아온 accessToken을 globalState에 저장하기
+    if (accessToken === undefined) {
+      alert("로그인에 실패했습니다! 다시 시도해 주세요!");
+    }
+    setAccessToken(accessToken);
+    localStorage.setItem("accessToken", accessToken);
+
+    // 3. 로그인 성공 페이지로 이동하기
+    router.push("/section23/23-04-login-localstorage-check-hoc-success");
+  };
+
+  return (
+    <>
+      이메일: <input type="text" />
+      <br />
+      비밀번호: <input type="password" />
+      <br />
+      <button onClick={onClickLogin}>로그인</button>
+    </>
+  );
+}
