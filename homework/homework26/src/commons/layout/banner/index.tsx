0a061c46e398ed9bed9da7921ce8fc3e93388612
pepper-sister <yuyu7123@naver.com>
2026-02-10 15:45:19 +0900

    refactor: 배열로 이미지 관리

--- homework/homework26/src/commons/layout/banner/index.tsx
+++ homework/homework26/src/commons/layout/banner/index.tsx
@@ -9,38 +9,24 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 export default function Banner() {
+  const array = new Array(3).fill("");
+
   return (
     <Swiper className={styles.banner__pagination} pagination={{ clickable: true }} modules={[Pagination]}>
-      <SwiperSlide className={styles.banner__style}>
-        <Image
-          src="/images/banner/banner1.png"
-          className={styles.banner__image}
-          alt="배너이미지"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-      </SwiperSlide>
-      <SwiperSlide className={styles.banner__style}>
-        <Image
-          src="/images/banner/banner2.png"
-          className={styles.banner__image}
-          alt="배너이미지"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-      </SwiperSlide>
-      <SwiperSlide className={styles.banner__style}>
-        <Image
-          src="/images/banner/banner3.png"
-          className={styles.banner__image}
-          alt="배너이미지"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-      </SwiperSlide>
+      {array.map((_, index) => {
+        return (
+          <SwiperSlide key={index} className={styles.banner__style}>
+            <Image
+              src={`images/banner/banner${index + 1}.png`}
+              className={styles.banner__image}
+              alt="배너이미지"
+              width={0}
+              height={0}
+              sizes="100vw"
+            />
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 }
