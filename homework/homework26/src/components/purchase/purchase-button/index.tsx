3c7d293ed03c3b281c4c0f06d08ccc94e0570a93
pepper-sister <yuyu7123@naver.com>
2026-02-10 17:20:13 +0900

    style: css cursor 추가

--- homework/homework26/src/components/purchase/purchase-button/index.tsx
+++ homework/homework26/src/components/purchase/purchase-button/index.tsx
@@ -6,13 +6,13 @@ export default function PurchaseButtonUI() {
   return (
     <div className="row__sort gap__16">
       <button
-        className={`${active ? "black__btn c__ffffff" : "bg__transparent c__525252"} padding__8__16 w__600`}
+        className={`${active ? "black__btn c__ffffff" : "bg__transparent c__525252"} padding__8__16 w__600 click`}
         onClick={() => setActive(!active)}
       >
         예약 가능 숙소
       </button>
       <button
-        className={`${!active ? "black__btn c__ffffff" : "bg__transparent c__525252"} padding__8__16  w__600`}
+        className={`${!active ? "black__btn c__ffffff" : "bg__transparent c__525252"} padding__8__16  w__600 click`}
         onClick={() => setActive(!active)}
       >
         예약 마감 숙소
