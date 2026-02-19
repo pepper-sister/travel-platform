ae3b956a8aa2b434a77f3bc35161e9aec1ee8e70
pepper-sister <yuyu7123@naver.com>
2026-02-19 19:52:17 +0900

    chore: 사용하지 않는 타입, 스토어 삭제

--- homework/homework26/src/components/boards-comment/boards-comment-write/types.ts
+++
@@ -1,6 +0,0 @@
-import { FetchBoardCommentsQuery } from "@/commons/graphql/graphql";
-
-export interface ICommentEditProps {
-  el?: FetchBoardCommentsQuery["fetchBoardComments"][0];
-  boardCommentId?: string;
-}
