39b2cb257cf72c72915b1c840152cfa259ce9e91
pepper-sister <yuyu7123@naver.com>
2026-02-27 22:14:25 +0900

    section25

---
+++ class-practice/my-app/src/commons/ui/25-01-input/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+
+export default function MyInput(props) {
+  const { register } = useFormContext();
+
+  return <input type={props.type} {...register(props.keyname)} />;
+}
