9042f008000288c18bea49f87849bfe915ec1bc0
pepper-sister <yuyu7123@naver.com>
2026-03-19 21:46:51 +0900

    chore: id 리턴 추가

--- homework/homework30/src/app/voucher/[travelproductId]/_components/queries.ts
+++ homework/homework30/src/app/voucher/[travelproductId]/_components/queries.ts
@@ -3,6 +3,7 @@ import { gql } from "@apollo/client";
 export const FETCH_TRAVEL_PRODUCT = gql`
   query fetchTravelproduct($travelproductId: ID!) {
     fetchTravelproduct(travelproductId: $travelproductId) {
+      _id
       name
       remarks
       contents
