40e9b7022332e39f7463be4e2305725f970a1bb5
pepper-sister <yuyu7123@naver.com>
2026-03-19 21:51:00 +0900

    feat: 포인트 구매 기능 추가

--- homework/homework30/src/app/voucher/[travelproductId]/_components/seller/index.tsx
+++ homework/homework30/src/app/voucher/[travelproductId]/_components/seller/index.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { useVoucherDetail } from "../hook";
+import { useSeller } from "./hook";
+import { Modal } from "antd";
+import PointUI from "../point";
 
 export default function SellerUI() {
   const { data } = useVoucherDetail();
+  const {
+    isModalOpen,
+    isShortageModal,
+    isChargeModal,
+    setIsChargeModal,
+    handleOk,
+    handleCharge,
+    handleCancel,
+    onClickPurchase,
+  } = useSeller();
 
   return (
     <div className="flex column__sort gap__24">
@@ -19,7 +32,9 @@ export default function SellerUI() {
             </li>
           </ul>
         </div>
-        <button className="bg__2974E5 br__8 padding__12__16 click c__ffffff">구매하기</button>
+        <button onClick={onClickPurchase} className="bg__2974E5 br__8 padding__12__16 click c__ffffff">
+          구매하기
+        </button>
       </div>
       <div className="br__8 padding__24 column__sort gap__12 bg__F2F2F2">
         <h4 className="f__20 w__700 l__28">판매자</h4>
@@ -34,6 +49,77 @@ export default function SellerUI() {
           <p className="l__20 c__333333">{data?.fetchTravelproduct.seller?.name}</p>
         </div>
       </div>
+
+      <Modal
+        centered
+        title={<h1 className="f__18 w__600">해당 숙박권을 구매 하시겠어요?</h1>}
+        style={{ padding: "24px 24px", textAlign: "center" }}
+        open={isModalOpen}
+        closable={false}
+        footer={
+          <div className="row__sort row__center gap__12">
+            <button
+              onClick={handleCancel}
+              className="f__14 l__20 click"
+              style={{
+                width: "120px",
+                padding: "8px 12px",
+                border: "1px solid #000000",
+                borderRadius: "8px",
+                backgroundColor: "#ffffff",
+              }}
+            >
+              취소
+            </button>
+            <button
+              onClick={handleOk}
+              className="f__14 l__20 c__ffffff click"
+              style={{ width: "120px", padding: "8px 12px", borderRadius: "8px", backgroundColor: "#2974E5" }}
+            >
+              구매
+            </button>
+          </div>
+        }
+      >
+        <p className="f__14 l__20 c__333333">해당 숙박권은 포인트로만 구매 가능합니다.</p>
+      </Modal>
+
+      <Modal
+        centered
+        title={<h1 className="f__18 w__600">포인트 부족</h1>}
+        style={{ padding: "24px 24px", textAlign: "center" }}
+        open={isShortageModal}
+        closable={false}
+        footer={
+          <div className="row__sort row__center gap__12">
+            <button
+              onClick={handleCancel}
+              className="f__14 l__20 click"
+              style={{
+                width: "120px",
+                padding: "8px 12px",
+                border: "1px solid #000000",
+                borderRadius: "8px",
+                backgroundColor: "#ffffff",
+              }}
+            >
+              취소
+            </button>
+            <button
+              onClick={handleCharge}
+              className="f__14 l__20 c__ffffff click"
+              style={{ width: "120px", padding: "8px 12px", borderRadius: "8px", backgroundColor: "#2974E5" }}
+            >
+              충전
+            </button>
+          </div>
+        }
+      >
+        <p className="f__14 l__20 c__333333">포인트가 부족합니다.</p>
+        <p className="f__14 l__20 c__333333">포인트 충전 후 구매하세요.</p>
+      </Modal>
+
+      <PointUI isChargeModal={isChargeModal} setIsChargeModal={setIsChargeModal} handleCancel={handleCancel} />
     </div>
   );
 }
