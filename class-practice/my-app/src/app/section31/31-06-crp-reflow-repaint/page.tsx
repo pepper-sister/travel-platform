13c30269b6679b7e33fed0c8a10ba8c54cb6f46d
pepper-sister <yuyu7123@naver.com>
2026-03-22 16:35:08 +0900

    section31

---
+++ class-practice/my-app/src/app/section31/31-06-crp-reflow-repaint/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./styles.module.css";
+
+export default function CrpReflowRepaintPage() {
+  return (
+    <>
+      <div className={styles.myreflow}>리플로우 연습!</div>
+      <div className={styles.myrepaint}>리페인트 연습!</div>
+    </>
+  );
+}
