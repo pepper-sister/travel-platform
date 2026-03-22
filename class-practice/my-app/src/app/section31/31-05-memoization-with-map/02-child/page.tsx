26c8cc64ef606a56603cc308413452dcc350a0e6
pepper-sister <yuyu7123@naver.com>
2026-03-22 11:38:52 +0900

    section31

---
+++ class-practice/my-app/src/app/section31/31-05-memoization-with-map/02-child/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { memo } from "react";
+
+function Word(props) {
+  console.log("자식이 렌더링 됩니다!", props.el);
+
+  return <span>{props.el}</span>;
+}
+
+export default memo(Word);
