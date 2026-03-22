b23cb2371b7a6f89f868b5beff0c065f09f61da4
pepper-sister <yuyu7123@naver.com>
2026-03-22 11:03:52 +0900

    section31

---
+++ class-practice/my-app/src/app/section31/31-04-re-render-with-memoization-advanced-with-child/02-child/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { memo } from "react";
+
+function ChildPage(props) {
+  console.log("자식이 렌더링 됩니다!");
+
+  return (
+    <>
+      <div>===============================</div>
+      <h1>저는 자식 컴포넌트 입니다!!!</h1>
+      <div>===============================</div>
+    </>
+  );
+}
+
+export default memo(ChildPage);
