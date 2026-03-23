20e0ef75f1247c02fcf10112ada23c0415a478cc
pepper-sister <yuyu7123@naver.com>
2026-03-23 23:20:16 +0900

    section31

---
+++ supplement/section31/file-prefetch-preload/src/app/31-01-file-prefetch-preload-moved/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function FilePrefetchPreloadMovedPage() {
+  return <div>이동하였습니다</div>;
+}
---
+++ supplement/section31/file-prefetch-preload/src/app/31-01-file-prefetch-preload/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Link from "next/link";
+
+export default function FilePrefetchPreloadPage() {
+  return <Link href="/31-01-file-prefetch-preload-moved">클릭하여 이동하기</Link>;
+}
