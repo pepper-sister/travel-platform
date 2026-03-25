6f507e3f9568a0fffece4215e9f3b99efd475dca
pepper-sister <yuyu7123@naver.com>
2026-03-25 23:54:45 +0900

    feat: 게시글 좋아요 기능 추가

--- homework/homework35/src/app/boards/[boardId]/_components/like-dislike/index.tsx
+++ homework/homework35/src/app/boards/[boardId]/_components/like-dislike/index.tsx
@@ -2,15 +2,18 @@ import styles from "./styles.module.css";
 import HeartBrokenOutlinedIcon from "@mui/icons-material/HeartBrokenOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import { IFetchBoardData } from "../types";
+import { useLikeDislike } from "./hook";
 
 export default function LikeDislikeUI({ data }: IFetchBoardData) {
+  const { onClickLike, onClickDislike } = useLikeDislike();
+
   return (
     <div className="row__sort row__center gap__24">
-      <div className="column__sort column__center gap__4">
+      <div onClick={onClickLike} className="column__sort column__center gap__4 click">
         <HeartBrokenOutlinedIcon className={styles.detail__bad__icon} />
         <p className="f__14 w__400 l__20 c__5F5F5F">{data?.likeCount}</p>
       </div>
-      <div className="column__sort column__center gap__4">
+      <div onClick={onClickDislike} className="column__sort column__center gap__4 click">
         <FavoriteBorderOutlinedIcon className={styles.detail__good__icon} />
         <p className="f__14 w__400 l__20 c__F66A6A">{data?.dislikeCount}</p>
       </div>
