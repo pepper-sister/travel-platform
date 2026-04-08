e88dd6366003b14f35891cb72a56a4978074ecd6
pepper-sister <yuyu7123@naver.com>
2026-04-08 16:51:07 +0900

    section35

---
+++ class-practice/my-app/__tests__/35-01-jest/banana.test.ts
@@ -0,0 +1,18 @@
+import { add } from "@/app/section35/35-01-jest/banana";
+
+it("더하기 잘 되는지 테스트 해보기", () => {
+  const result = add(3, 5);
+  expect(result).toBe(8);
+});
+
+// describe("나만의 테스트 그룹 만들기", () => {
+//   it("더하기 테스트", () => {
+
+//   })
+
+//   it("빼기 테스트", () => {
+
+//   })
+
+//   // ...
+// })
