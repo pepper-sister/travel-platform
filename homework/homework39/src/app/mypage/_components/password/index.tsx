9dd199ede1d8c2f0e52a327b92bfef83e27d4147
pepper-sister <yuyu7123@naver.com>
2026-04-21 17:31:19 +0900

    style: 모달 가운데 정렬

--- homework/homework39/src/app/mypage/_components/password/index.tsx
+++ homework/homework39/src/app/mypage/_components/password/index.tsx
@@ -43,6 +43,7 @@ export default function PasswordUI() {
         비밀번호 변경
       </button>
       <Modal
+        centered
         title="비밀번호 변경 완료"
         open={isModalOpen}
         onOk={handleOk}
