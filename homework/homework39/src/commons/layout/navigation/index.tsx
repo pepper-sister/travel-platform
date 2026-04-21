be10a310b96d42ec3f2c8a6b4ef5684919d1658f
pepper-sister <yuyu7123@naver.com>
2026-04-21 17:30:09 +0900

    feat: 내비게이션바 프로필 드롭다운 추가

--- homework/homework39/src/commons/layout/navigation/index.tsx
+++ homework/homework39/src/commons/layout/navigation/index.tsx
@@ -1,13 +1,93 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useAccessTokenStore } from "@/commons/stores/access-token";
+import { Dropdown, DropdownProps, MenuProps } from "antd";
+import { useLoggedIn } from "@/commons/libraries/is-logged-in";
+import { useState } from "react";
+import PointUI from "@/app/voucher/[travelproductId]/_components/point";
+import { useMutation } from "@apollo/client/react";
+import { LogoutUserDocument } from "@/commons/graphql/graphql";
 
 export default function NavigationUI() {
+  const { data } = useLoggedIn();
+  const { accessToken } = useAccessTokenStore();
+  const [isChargeModal, setIsChargeModal] = useState(false);
+  const router = useRouter();
   const pathname = usePathname();
   const isActive = (path: string) => pathname === path;
-  const { accessToken } = useAccessTokenStore();
+  const [logoutUser] = useMutation(LogoutUserDocument);
+
+  const handleCancel = () => {
+    setIsChargeModal(false);
+  };
+
+  const handleMenuClick: MenuProps["onClick"] = async (e) => {
+    if (e.key === "0" || e.key === "1") {
+      router.push("/mypage");
+    } else if (e.key === "2") {
+      setIsChargeModal(true);
+    } else if (e.key === "3") {
+      try {
+        await logoutUser();
+
+        location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const items: MenuProps["items"] = [
+    {
+      key: "0",
+      label: `${data?.fetchUserLoggedIn.name}`,
+      icon: (
+        <Image
+          className="br__100 bg__E4E4E4"
+          src="/images/navigation/profile.png"
+          width={40}
+          height={40}
+          alt="profile"
+        />
+      ),
+      onClick: handleMenuClick,
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "1",
+      label: `${data?.fetchUserLoggedIn.userPoint?.amount.toLocaleString() || 0} P`,
+      icon: <Image src="/images/navigation/wallet.png" width={24} height={24} alt="wallet" />,
+      onClick: handleMenuClick,
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "2",
+      label: "포인트 충전",
+      icon: <Image src="/images/navigation/charge.png" width={16} height={16} alt="charge" />,
+      onClick: handleMenuClick,
+    },
+    {
+      key: "3",
+      label: "로그아웃",
+      icon: <Image src="/images/navigation/logout.png" width={16} height={16} alt="logout" />,
+      danger: true,
+      onClick: handleMenuClick,
+    },
+  ];
+
+  const sharedProps: DropdownProps = {
+    menu: { items, style: { padding: "16px" } },
+    placement: "bottomRight",
+    align: {
+      offset: [0, -32],
+    },
+  };
 
   return (
     <div className="row__sort row__center">
@@ -53,10 +133,14 @@ export default function NavigationUI() {
               width={40}
               height={40}
             />
-            <Image src="/images/navigation/down_arrow.png" alt="down" width={24} height={24} />
+            <Dropdown {...sharedProps}>
+              <Image className="click" src="/images/navigation/down_arrow.png" alt="down" width={24} height={24} />
+            </Dropdown>
           </div>
         )}
       </div>
+
+      <PointUI isChargeModal={isChargeModal} setIsChargeModal={setIsChargeModal} handleCancel={handleCancel} />
     </div>
   );
 }
