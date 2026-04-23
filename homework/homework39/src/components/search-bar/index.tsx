828f1147a2a4383b91db145a6f102d5e4c70865f
pepper-sister <yuyu7123@naver.com>
2026-04-23 22:07:56 +0900

    style: 검색바 모바일 반응형 추가

--- homework/homework39/src/components/search-bar/index.tsx
+++ homework/homework39/src/components/search-bar/index.tsx
@@ -21,8 +21,14 @@ export default function SearchBarUI() {
         rowGap: "16px",
       }}
     >
-      <div className="row__sort gap__16 flex__grow">
-        <div className="br__8 padding__12 row__sort gap__8 bg__F2F2F2">
+      <div
+        className="row__sort gap__16 flex__grow"
+        style={{
+          flexWrap: "wrap",
+          rowGap: "12px",
+        }}
+      >
+        <div className={`${styles.search__input__section} br__8 padding__12 row__sort gap__8 bg__F2F2F2`}>
           <Image src="/images/search-bar/search.png" alt="검색" width={24} height={24} />
           <input
             type="text"
@@ -35,20 +41,23 @@ export default function SearchBarUI() {
           ""
         ) : (
           <RangePicker
-            className={`${styles.search__date} flex__grow click bg__F2F2F2`}
+            className={`${styles.search__date} click bg__F2F2F2`}
             style={{ maxWidth: "289px" }}
             format={dateFormat}
             onChange={onChangedate}
           />
         )}
 
-        <button className="bg__000000 br__8 padding__12__16 click f__18 w__600 c__ffffff" onClick={onClickSearch}>
+        <button
+          className={`${styles.search__btn} bg__000000 br__8 padding__12__16 click f__18 w__600 c__ffffff`}
+          onClick={onClickSearch}
+        >
           검색
         </button>
       </div>
       <Link
         href={isVoucher ? "/voucher/new" : "/boards/new"}
-        className="bg__2974E5 br__8 padding__12__16 click row__sort gap__8 f__18 w__600 c__ffffff"
+        className={`${styles.submit__btn} bg__2974E5 br__8 padding__12__16 click row__sort gap__8 f__18 w__600 c__ffffff`}
         style={{
           marginLeft: "auto",
         }}
