2241e6ef963ae894b63093f5f2d6ed73841d2081
pepper-sister <yuyu7123@naver.com>
2026-04-29 20:58:12 +0900

    refactor: 이벤트 배너 이미지로 변경

--- homework/homework39/src/app/voucher/page.tsx
+++ homework/homework39/src/app/voucher/page.tsx
@@ -2,12 +2,12 @@
 
 import SearchBarUI from "@/components/search-bar";
 import RecommendUI from "./_components/recommend";
-import EventUI from "./_components/event";
 import ToggleUI from "./_components/toggle";
 import MenuUI from "./_components/menu";
 import ListUI from "./_components/list";
 import { Suspense, useEffect } from "react";
 import { useVoucherStore } from "@/commons/stores/voucher";
+import Image from "next/image";
 
 function VoucherContents() {
   const { setIsVoucher } = useVoucherStore();
@@ -19,7 +19,7 @@ function VoucherContents() {
     <div className="row__sort row__center">
       <div className="width__1280px padding__40__20 column__sort gap__64">
         <RecommendUI />
-        <EventUI />
+        <Image width={0} height={0} className="width__100 height__auto" src="/images/voucher/banner.png" alt="banner" />
         <div className="column__sort gap__24">
           <h2 className="f__28 w__700 l__36">여기에서만 예약할 수 있는 숙소</h2>
           <ToggleUI />
