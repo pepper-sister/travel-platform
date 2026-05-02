ce40a216ddafcb68185ea52380b096085b7c63ba
pepper-sister <yuyu7123@naver.com>
2026-05-02 22:37:50 +0900

    style: 숙박권 테마 메뉴 모바일 반응형 추가

--- homework/homework39/src/app/voucher/_components/menu/index.tsx
+++ homework/homework39/src/app/voucher/_components/menu/index.tsx
@@ -2,14 +2,15 @@
 
 import Image from "next/image";
 import { useMenu } from "./hook";
+import styles from "./styels.module.css";
 
 export default function MenuUI() {
   const { reserveIcon } = useMenu();
 
   return (
-    <div className="padding__16__0 row__sort row__between">
+    <div className={styles.menu__section}>
       {reserveIcon.map(([key, value]) => (
-        <div key={key} className="width__100px click column__sort column__center gap__8">
+        <div key={key} className="click column__sort column__center gap__8">
           <Image src={`/images/voucher/voucher-menu/${key}.png`} alt={value} width={40} height={40} />
           <p className="c__333333" style={{ whiteSpace: "nowrap" }}>
             {value}
